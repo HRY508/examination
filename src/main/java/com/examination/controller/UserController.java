e767d226f2ec0102c2224fab7392985756c88245
He <508816739@qq.com>
2022-09-28 19:30:32 +0800

    管理员列表、用户 列表

---
+++ src/main/java/com/examination/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.examination.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Description
+ * @Author he
+ * @Data 2022/9/28 19:14
+ */
+@Controller
+public class UserController {
+
+}
