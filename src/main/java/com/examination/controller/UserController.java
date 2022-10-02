caa607bbcdb5996c049d10191adee8b0ca370f30
15236129514 <1625378509@qq.com>
2022-10-02 17:49:11 +0800

    删除

--- src/main/java/com/examination/controller/UserController.java
+++ src/main/java/com/examination/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.examination.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.examination.bean.User;
 import com.examination.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @Description
@@ -34,22 +40,33 @@ public class UserController {
         request.setAttribute("page", result);
         return "admin/user_list";
     }
-    @RequestMapping("/admin/searchName")
-    public String searchSubmit(@RequestParam(value = "searchName",required = false,defaultValue = "")String searchName,
+
+    @RequestMapping("/admin/searchByList")
+    public String searchSubmit(@RequestParam(value = "userName",required = false,defaultValue = "")String userName,
                                HttpServletRequest request,
                                @RequestParam(required = false, defaultValue = "1", value = "pn") Integer pn) {
 //        IPage<User> page = userService.searchByPage(pn, 5, searchName);
         Page<User> userPage=new Page<>(pn,2);
         QueryWrapper<User> queryWrapper=new QueryWrapper<>();
-        queryWrapper.like("userName",searchName).or().like("realName",searchName);
+        queryWrapper.like("user_name",userName);
         Page<User> result = userService.page(userPage, queryWrapper);
-        request.setAttribute("jumpUrl", "/admin/searchName?searchName="+searchName+"&pn=");
+        request.setAttribute("jumpUrl", "/admin/searchName?userName="+userName+"&pn=");
         request.setAttribute("page", result);
         return "admin/user_list";
     }
 
 
-
-
+    // 批量删除
+    @RequestMapping("admin/deleteCount")
+    public String deleteCount(Integer[] ids){//ids是复选框名字
+        userService.removeByIds(Arrays.asList(ids));
+        return "redirect:/admin/userList";
+    }
+    //删除
+    @GetMapping("admin/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        userService.removeById(id);
+        return "redirect:/admin/userList";
+    }
 
 }
