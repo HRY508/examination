665ad37ee7d625edf9c5d217ab7c55fb19ca2faf
15236129514 <1625378509@qq.com>
2022-10-10 11:01:33 +0800

    退出登录

--- src/main/java/com/examination/controller/PageController.java
+++ src/main/java/com/examination/controller/PageController.java
@@ -3,7 +3,13 @@ import com.alibaba.fastjson.JSON;
 import com.examination.bean.QuestionEditVM;
 import com.examination.bean.QuestionItemObject;
 import com.examination.bean.QuestionObject;
+import com.examination.bean.User;
 import com.examination.service.QuestionEditVMService;
+import com.examination.utils.GlobalUserUtil;
+import lombok.RequiredArgsConstructor;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,5 +30,25 @@ public class PageController {
         return "admin/index";
     }
 
+    /**
+     * 管理员退出登录
+     * @return
+     */
+    @RequiresAuthentication
+    @RequestMapping("/admin/logout")
+    public String logout(){
+        Subject subject = SecurityUtils.getSubject();
+        if(subject.isAuthenticated()){
+            subject.logout();
+        }
+        return "login";
+    }
+
+    /**
+     * 修改密码
+     */
+//    public String updatePassword(){
+//        User user = GlobalUserUtil.getUser();
+//    }
 
 }
